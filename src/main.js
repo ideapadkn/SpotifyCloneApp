fa37014c09259d293968b47ac9cf6187fdeae145
Kamron or Камрон <kamrush6429@gmail.com>
2023-07-16 22:56:09 +0500

    add pinia-plugin-persistedstate, uniqolor, tailwind, vue-materials-icons

--- src/main.js
+++ src/main.js
@@ -3,12 +3,17 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
