715038b0a039d5d262f6c21ea1372524a0d0cae0
Kamron or Камрон <kamrush6429@gmail.com>
2023-07-17 10:25:28 +0500

    main layout section finish

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue';
 import { RouterLink, RouterView } from 'vue-router';
+import MenuItem from './components/MenuItems.vue'
 
 import ChevronUp from 'vue-material-design-icons/ChevronUp.vue';
 import ChevronDown from 'vue-material-design-icons/ChevronDown.vue';
@@ -63,10 +64,70 @@ let openMenu = ref(false)
     </div>
 
     <div
-      id="sideNow"
+      id="sideNav"
       class="h-[100%] p-6 w-[240px] fixed z-50 bg-black"
     >
+      <RouterLink to="/">
+        <img width="125" src="../public/images/icon/spotify-logo.png" alt="logo">
+      </RouterLink>
+      <div class="my-8"></div>
+      <ul>
+        <RouterLink to="/">
+          <MenuItem 
+            class="ml-[1px]"
+            :iconSize="23" 
+            name="Home" 
+            iconString="home" 
+            pageUrl="/" 
+          />
+        </RouterLink>
+        <RouterLink to="/search">
+          <MenuItem 
+            class="ml-[1px]"
+            :iconSize="24" 
+            name="Search" 
+            iconString="search"
+            pageUrl="/search" 
+          />
+        </RouterLink>
+        <RouterLink to="/library">
+          <MenuItem 
+            class="ml-[2px]"
+            :iconSize="23" 
+            name="Library" 
+            iconString="library"
+            pageUrl="/library" 
+          />
+        </RouterLink>
+        <div class="py-3.5"></div>
+        <MenuItem :iconSize="24" name="Create Playlist" iconString="playlist" pageUrl="/playlist"/>
+        <MenuItem class="-ml-[1px]" :iconSize="27" name="Likes Songs" iconString="liked" pageUrl="/liked"/>
+      </ul>
+      <div class="border-b border-b-gray-700"></div>
+      <ul>
+        <li class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white">My PLaylist #1</li>
+        <li class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white">My PLaylist #2</li>
+        <li class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white">My PLaylist #3</li>
+        <li class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white">My PLaylist #4</li>
+      </ul>
+    </div>
 
+    <div
+      class="
+        fixed
+        right-0
+        top-0
+        w-[calc(100%-240px)]
+        overflow-auto
+        h-full
+        bg-gradient-to-b
+        from-[#1c1c1c]
+        to-black
+      "
+    >
+      <div class="mt-[70px]"></div>
+        <RouterView />
+      <div class="mb-[100px]"></div>
     </div>
 
   </div> 
