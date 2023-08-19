fc452b2c4684e80c5f6e09bed599b46814fa22d5
Kamron or Камрон <kamrush6429@gmail.com>
2023-08-19 16:55:58 +0500

    fix router bugs

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import SearchView from "../views/SearchView.vue";
 import LibraryView from "../views/LibraryView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory("#"),
   routes: [
     {
       path: "/",
